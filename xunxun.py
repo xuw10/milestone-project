8de178ad5de2061134e8941386e761d8c5f54bcb
Xun Wang <wangxun.pitt@gmail.com>
2015-12-16 15:23:41 -0500

    init

--- xunxun.py
+++ xunxun.py
@@ -1,45 +1,52 @@
-from flask import Flask, render_template, request, redirect
-from jinja2 import Template
-import pandas as pd
-from bokeh.plotting import figure,output_notebook,show
-from bokeh import embed
-from flask import Flask,request,session,g,redirect,url_for,abort,render_template,flash
-import time
-from datetime import *
-import numpy as np
-from bokeh.plotting import *
-from bokeh.resources import CDN
-from bokeh.embed import file_html,components 
-from bokeh.palettes import Spectral11
-output_file('figures.html')
-app = Flask(__name__)
+import flask
 
-@app.route('/')
+from bokeh.embed import components
+from bokeh.plotting import figure
+from bokeh.resources import INLINE
+from bokeh.util.string import encode_utf8
+
+app = flask.Flask(__name__)
+
+colors = {
+    'Black': '#000000',
+    'Red':   '#FF0000',
+    'Green': '#00FF00',
+    'Blue':  '#0000FF',
+}
+
+def getitem(obj, item, default):
+    if item not in obj:
+        return default
+    else:
+        return obj[item]
+@app.route("/")
+def polynomial():
+    args = flask.request.args
+    color = colors[getitem(args, 'color', 'Black')]
+    
+    #####NEED TO MODIFY THIS PART!!!###########
+    _from = int(getitem(args, '_from', 0))
+    to = int(getitem(args, 'to', 200))
+    x = list(range(_from, to + 1))
+    fig = figure(title="Historical prices of different stocks")
+    fig.line(x, [i ** 3 for i in x], color=color, line_width=2)
+    #######END MODIFIED PART!####################
+    js_resources = INLINE.render_js()
+    css_resources = INLINE.render_css()
+    script, div = components(fig, INLINE)
+    html = flask.render_template(
+        'embed.html',
+        plot_script=script,
+        plot_div=div,
+        js_resources=js_resources,
+        css_resources=css_resources,
+        color=color,
+        _from=_from,
+        to=to
+    )
+    return encode_utf8(html)
 def main():
-   return redirect('/index')
-#@app.route('/')
-#def indexPage():
-#    df = pd.read_csv("YAHOO-INDEX_GSPC.csv",header=None,names=['Date','Open','High','Low','Close','Volume','Adj Close'],index_col='Date')
-#    numlines=len(df.columns)
-#    mypalette=Spectral11[0:numlines]
-#    yys = []
-#    for name in df:
-#        yy = df[name].values
-#        yys.append(yy)
-#    TOOLS = "pan,wheel_zoom,box_zoom,reset,resize"
-#    p = figure(data_frame,tools=TOOLS,width=300,height=300,title='Data from Quandle WIKI set',x_axis_label='date',x_axis_type='datetime')
-#    my_d_np=[datetime.strptime(x, '%m/%d/%y') for x in df.index.values]
-#    p.line(my_d_np,yys[0],legend="Open",line_width=2,line_color="red")
-#    #p.line(my_d_np,yys[1],legend="High",line_width=2,line_color="blue")
-#    #p.line(my_d_np,yys[2],legend="Low",line_width=2,line_color="green")
-#    #p.line(my_d_np,yys[3],legend="Close",line_width=2,line_color="orange")
-#    #p.line(my_d_np,yys[5],legend="Adj Close",line_width=2,line_color="black")
-#    #figJS,figDiv = components(p,CDN)
-#    script,div=embed.components(p)
-#    return render_template('figures.html',script=script,div=div)
-@app.route('/index')
-def index():
-  return render_template('index.html')
-
-if __name__ == '__main__':
-  app.run(port=33507)
+    #app.debug = True
+    app.run()
+if __name__ == "__main__":
+    main()
